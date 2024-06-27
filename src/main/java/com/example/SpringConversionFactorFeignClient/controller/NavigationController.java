000b995c0abef1e7ca114bad269a077473cbb284
abhikgh <abhikgh.work@gmail.com>
2024-06-27 10:52:00 +0200

    Update NavigationController.java

--- src/main/java/com/example/SpringConversionFactorFeignClient/controller/NavigationController.java
+++ src/main/java/com/example/SpringConversionFactorFeignClient/controller/NavigationController.java
@@ -1,9 +1,8 @@
 package com.example.SpringConversionFactorFeignClient.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +15,19 @@ public class NavigationController {
     @Autowired
     private WebClient.Builder webClientBuilder;
 
+    //http://localhost:8071/getConversionFactorClient/AUD
+    @GetMapping(value = "/getConversionFactorClient/{countryCode}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Mono<String> convertCurrency(@PathVariable("countryCode") String countryCode) {
+
+        return webClientBuilder.build()
+                .get()
+                .uri("http://conversion-factor/eureka/v1/getConversionFactor/" + countryCode)
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .bodyToMono(String.class);
+
+    }
 
     // http://localhost:8071/convertCurrency/AUD
 	/*{
@@ -40,19 +52,5 @@ public class NavigationController {
         return new ResponseEntity<>(currency2, HttpStatus.OK);
     }*/
 
-    //http://localhost:8071/getConversionFactorClient/AUD
-    @GetMapping(value = "/getConversionFactorClient/{countryCode}" , produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Mono<String>> convertCurrency(@PathVariable("countryCode") String countryCode){
 
-            Mono<String> response =
-                                 webClientBuilder.build()
-                                .get()
-                                .uri("http://conversion-factor/eureka/v1/getConversionFactor/AUD")
-                                .retrieve()
-                                .bodyToMono(String.class)
-                                .map(s->s);
-           return ResponseEntity.status(HttpStatus.OK)
-                                .body(response);
-
-    }
 }
