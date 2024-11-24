325437e30efd841742ff52bea707f7ebed0b09de
Abhik <abhikgh.work@gmail.com>
2024-11-24 19:20:26 +0530

    update

--- src/main/java/com/example/SpringConversionFactorFeignClient/configuration/WebClientConfig.java
+++ src/main/java/com/example/SpringConversionFactorFeignClient/configuration/WebClientConfig.java
@@ -3,9 +3,11 @@ package com.example.SpringConversionFactorFeignClient.configuration;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @LoadBalancerClient(name = "conversion-factor", configuration = ServiceInstanceConfig.class)
+@Configuration
 class WebClientConfig {
 
     @LoadBalanced
